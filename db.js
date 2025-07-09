96f82b0c6d621e682514790bbe759e51fb4f47cf
Eddie <Eddie000321@outlook.com>
2025-07-08 20:57:35 -0400

    docker compose up --build -d

--- db.js
+++ db.js
@@ -1,1 +1,10 @@
-const {pool} = require('pg');
+const { Pool } = require('pg') // postgres interaction package for node.js
+const pool = new Pool({
+    host: 'db',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'db123',
+})
+
+module.exports = pool
